30490e52efadcabef3ecac0747c4813e206a0837
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-08-05 15:49:36 -0700

    completed category routes and completed the model relationships in the index js file within the models folder

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "catergory_id",
+  foreignKey: "category_id",
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -19,6 +19,10 @@ ProductTag.belongsTo(Product, {
 ProductTag.belongsTo(Tag, {
   foreignKey: "tag_id",
 });
+
+Product.hasMany(ProductTag);
+
+Tag.hasMany(ProductTag);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
